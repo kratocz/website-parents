db83e9b0e422435ceb85dc3d35bba3debcbad924
Petr Kratochvíl <krato@krato.cz>
2023-12-27 00:48:35 +0100

    LinkPage improved; websiteSections.tsv

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/old-home',
       name: 'home',
       component: HomeView
     },
@@ -17,7 +17,8 @@ const router = createRouter({
       component: HowToControlPhoneView
     },
     {
-      path: '/addresses',
+      //path: '/addresses',
+      path: '/',
       name: 'addresses',
       component: AddressesView
     },
